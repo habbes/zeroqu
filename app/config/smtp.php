2cc3c59ab558213898664f3b7eddcfafaab24f73
grafitto <nderitukelvin19@gmail.com>
2015-07-01 10:37:09 +0300

    Changed Passwords
    Changed Clements passwords to my passwords

--- app/config/smtp.php
+++ app/config/smtp.php
@@ -4,8 +4,8 @@ $smtp = [
 	"host" => "smtp.mail.yahoo.com",
 	"port" => "465",
 	"security" => "ssl",
-	"uname" => "haby_habbes@yahoo.com",
-	"pass" => "58sisnax13",
-	"from" => "haby_habbes@yahoo.com",
+	"uname" => "nderitukelvin19@yahoo.com",
+	"pass" => "blackphantom",
+	"from" => "nderitukelvin19@yahoo.com",
 	"from_name" => "Elections Manager"
 ];
