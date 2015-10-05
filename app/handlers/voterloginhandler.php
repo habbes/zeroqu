78a2d3e8db04c20d71f5d3efc880da12713e7a67
Habbes <haby_habbes@outlook.com>
2015-10-06 00:14:04 +0300

    Implemented Voter page post handler

--- app/handlers/voterloginhandler.php
+++ app/handlers/voterloginhandler.php
@@ -3,15 +3,30 @@
 class VoterLoginHandler extends ElectionHandler
 {
 	
-	public function get($electionName)
+	private function showPage()
 	{
-		$this->viewParams->voterId = $this->getVar('id');
 		$this->renderView('VoterLogin');
 	}
 	
-	public function post()
+	public function get($electionName)
+	{
+		$this->viewParams->voterId = $this->getVar('id');
+		$this->showPage();
+	}
+	
+	public function post($electionName)
 	{
 		
+		$vId = trim($this->postVar("id"));
+		$password = trim($this->postVar("password"));
+		
+		if($voter = Voter::login($vId, $this->election->getName(), $password)){
+			Login::voterLogin($voter);
+			$this->localRedirect("home");
+		}
+		$this->viewParams->voterId = $vId;
+		$this->viewParams->loginError = "Incorrect Voter ID or Password.";
+		$this->showPage();
 	}
 	
 	
