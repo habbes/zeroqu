8c95409b6c18277cfe92238386d001e38c93845d
Habbes <haby_habbes@outlook.com>
2015-10-06 02:21:58 +0300

    Voter logout redirects to voter login page

--- app/handlers/logouthandler.php
+++ app/handlers/logouthandler.php
@@ -4,7 +4,13 @@ class LogoutHandler extends RequestHandler
 {
 	public function get()
 	{
+		$dest = "home";
+		if(Login::isVoterLoggedIn()){
+			$voter = Login::getVoter();
+			$dest = $voter->getElection()->getName() . "/voter-login?id="
+					. $voter->getId();	
+		}
 		Login::logout();
-		$this->localRedirect("home");
+		$this->localRedirect($dest);
 	}
 }
