917484fe3b11187ad877fb91e34cf247f3458bee
Habbes <haby_habbes@outlook.com>
2015-10-06 02:48:07 +0300

    Made menu collapsible

--- app/views/_templates/home.php
+++ app/views/_templates/home.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-collapse" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -11,7 +11,7 @@
       <a class="navbar-brand" href="<?= URL_ROOT ?>">zero<b>Q</b>u</a>
     </div>
     
-    <div class="collapse navbar-collapse">
+    <div class="collapse navbar-collapse" id="nav-collapse">
     	<ul class="nav navbar-nav navbar-right">
     		<li><a href="#">Features</a></li>
     		<li><a href="#">Pricing</a></li>
