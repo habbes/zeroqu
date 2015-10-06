8931bead9d35371996e84a2f2333b8c1daefdf17
Habbes <haby_habbes@outlook.com>
2015-10-06 20:42:57 +0300

    Fixed admin menu collapse

--- app/views/_templates/admin_menu.php
+++ app/views/_templates/admin_menu.php
@@ -1,26 +1,3 @@
-
-<!-- <span class="menu-header"><a href="new-election"><span class="new-icon">+</span> New Election</a></span>
-<ul class="menu-list">
-	<?php 
-		foreach($data->elections as $election){
-			$url = $election->getName();
-		?>
-		<li>
-			<a class="election-link" href="<?=$url?>"><?= $election->getTitle() ?></a>
-			<?php if($data->election && $election->is($data->election)) {?>
-			<ul class="submenu">
-				<li><a href="<?=$url?>/settings">Settings</a></li>
-				<li><a href="<?=$url?>/positions">Positions</a></li>
-				<li><a href="<?=$url?>/candidates">Candidates</a></li>
-				<li><a href="<?=$url?>/voters">Voters</a></li>
-				<li><a href="<?=$url?>/results">Results</a>
-			</ul>
-			<?php }?>
-		</li>
-		<?php 
-	}?>
-</ul>
- -->
 <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
