468d6b7f2712dcf3ff9e30f5df0643c4a4725191
Habbes <haby_habbes@outlook.com>
2015-10-08 01:32:38 +0300

    Added .env

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ require_once "app/autoload.php";
 require_once "app/routes.php";
 require_once "app/urls.php";
 
+$dotenv = new Dotenv\Dotenv(DIR_ROOT);
+$dotenv->load();
+
 $url = isset($_GET['url'])? $_GET['url'] : '';
 
 $app = new Application($url, $routes);
