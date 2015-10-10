71250e8add31baa9e39d375a5fd31ef76bb90f7e
Habbes <haby_habbes@outlook.com>
2015-10-11 01:14:11 +0300

    Made URL_ROOT detect server port

--- app/urls.php
+++ app/urls.php
@@ -1,3 +1,3 @@
 <?php
 
-define("URL_ROOT", "http://" . $_SERVER['SERVER_NAME']);
+define("URL_ROOT", "http://" . $_SERVER['SERVER_NAME'] . ($_SERVER['SERVER_PORT'] != 80? ":".$_SERVER['SERVER_PORT'] : ''));
